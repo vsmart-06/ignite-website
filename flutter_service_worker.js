f9e97b510dac12893ca29d2d39d0563c7004de81
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-10-16 20:02:54 +0530

    Release 1.6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "782aefad8dde9e6c67fb2025cdf9b6b5",
-"/": "782aefad8dde9e6c67fb2025cdf9b6b5",
-"main.dart.js": "2dee096bde1f2beb297d4fc6753ee0c0",
+"index.html": "a51a2f4b78085ba798196c154c97cb4f",
+"/": "a51a2f4b78085ba798196c154c97cb4f",
+"main.dart.js": "ee48324949b498b1a1c5d1977f0a4900",
 "manifest.json": "4ab92e27b32b26850b3934a2eb8bfaa2",
 "version.json": "bff4a08b0ad19c77446fac34aeba7710"};
 // The application shell files that are downloaded before a service worker can
