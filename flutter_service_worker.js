1701d4fa9d45a598272791eec604d69dfbe8dc4d
vsmart-06 <srivishnuvusirikala@gmail.com>
2023-11-01 11:10:46 +0530

    Release 3.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const MANIFEST = 'flutter-app-manifest';
-const TEMP = 'flutter-temp-cache-3.1';
-const CACHE_NAME = 'flutter-app-cache-3.1';
+const TEMP = 'flutter-temp-cache-3.2';
+const CACHE_NAME = 'flutter-app-cache-3.2';
 
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6d67aa4f2463a9bb1d64e98e751ba37e",
-"/": "6d67aa4f2463a9bb1d64e98e751ba37e",
-"main.dart.js": "475b394e65d607c195ad4a71bad357a4",
+"index.html": "5ad31294e4c6362d58e07202f7b88fbc",
+"/": "5ad31294e4c6362d58e07202f7b88fbc",
+"main.dart.js": "e6a82bd41098ca3323066ba0ec731b29",
 "manifest.json": "4ab92e27b32b26850b3934a2eb8bfaa2",
 "version.json": "bff4a08b0ad19c77446fac34aeba7710"};
 // The application shell files that are downloaded before a service worker can
